62cb213bad9679dbd618b99c77bbc73790c3c6cb
itisfine11 <realdd521@gmail.com>
2025-01-15 09:11:25 +0500

    edited next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/:path*", // Match all paths
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://ace-ai-ashen.vercel.app",
+            value: "https://ace-ai-ashen.vercel.app", // Your frontend URL
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT", // Allowed methods
           },
           {
             key: "Access-Control-Allow-Headers",
             value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version", // Allowed headers
           },
         ],
       },
     ];
   },
 };
-
-export default nextConfig;
